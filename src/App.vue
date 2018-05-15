d53e9cd40f84f513a4c87b2dcd9d32db4749ae7c
hyobum <hyob1203@naver.com>
2018-05-16 00:46:51 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="true">
+        <h1>Hello!</h1>
+        <h2>Vue.JS</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+    .h2 {
+        color: white;
+        background-color: black;
+    }
+</style>
